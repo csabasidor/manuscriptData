d5e2be4ced1697f25811de13bb876728014892e0
cs_sido <csaba.sidor@gmail.com>
2022-12-14 12:43:40 +0100

    Update callCR3804MR.py

--- callCR3804MR.py
+++ callCR3804MR.py
@@ -15,31 +15,33 @@ from collections import OrderedDict
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1500)
 
-#All the data sets with the API may be found at: https://data.statistics.sk/api/v2/collection/
+#All data themes within the API may be found at: https://data.statistics.sk/api/v2/collection/
 base_uri_to_set = 'https://data.statistics.sk/api/v2/dataset/'
+#This version of the script is intended only for the set variable "data_set_id".
 data_set_id = 'cr3804mr'
 response_lang = '?lang=en'
 
-#ENTER DB CREDENTIALS for LOADING into a SQL based DB to the "engine" variable.
-#If one does not itend to load the response data into a SQL based DB, the called data may be exported into a table (command ).
-
+#For loading the response data into a PostrgeSQL DB, please insert necessary credentials into the vaiable "engine".
 #engine = create_engine('postgresql+psycopg2://USERNAME:PASSWORD@HOSTNAME:PORTNUMBER/DBNAME')
+#If one does not itend to load the response data into DB,the response data may be exported as an excel file (command LINE).
 
-#For the selection of a different or adding additional administrative units please use comma as separators within the list_of_nuts variable.
-#Additional administrative units for the given data topic may be found: https://data.statistics.sk/api/v2/dimension/cr3804mr/nuts14?lang=en
+#For the selection of a different, or adding additional administrative units, please use comma as separators within the variable "list_of_nuts".
 list_of_nuts = ['SK0422_0425']
+#Additional administrative units for the given data topic may be found: https://data.statistics.sk/api/v2/dimension/cr3804mr/nuts14?lang=en
 
-#For the selection of a different year please change the value for value_id variable
+#For the selection of a different year please change the value for variable "value_id".
 year_id = '2021'
-#For excluding any month, please delete their value and separator within list of list_of_months variable.
-list_of_months = '1.,2.,3.,4.,5.,6.,7.,8.,9.,10.,11.,12.'
-#For excluding any indicator, please delete their value and separator within list of list_of_indicators variable.
-#List of indicators for the given data topic may be found: https://data.statistics.sk/api/v2/dimension/cr3804mr/cr3804mr_ukaz?lang=en
-list_of_indicators = 'UKAZ04, UKAZ07, UKAZ10'
-#For excluding any dimension (incoming market), please delete their value and separator within list of "list_of_dims" variable.
-#List of dimension for the given data topic may be found: https://data.statistics.sk/api/v2/dimension/cr3804mr/cr3804mr_dim3?lang=en
-list_of_dims = ['DIM01','DIM02','DIM03','DIM04','DIM05','DIM06','DIM07','DIM08','DIM09','DIM10','DIM11','DIM12','DIM13','DIM14','DIM15','DIM16','DIM17','DIM18','DIM19','DIM20','DIM21','DIM22','DIM23','DIM24','DIM25','DIM26','DIM27','DIM28','DIM29','DIM30','DIM31','DIM32','DIM33','DIM34','DIM35','DIM36','DIM37','DIM38','DIM39','DIM40','DIM41','DIM42','DIM43','DIM44','DIM45','DIM46','DIM47','DIM48','DIM49','DIM50','DIM51','DIM52','DIM53','DIM54','DIM55','DIM56','DIM57','DIM58','DIM59','DIM60','DIM61','DIM62','DIM63','DIM64','DIM65','DIM66','DIM67','DIM68','DIM69','DIM70','DIM71','DIM72','DIM73','DIM74','DIM75','DIM76','DIM77','DIM78','DIM79','DIM80','DIM81','DIM82','DIM83','DIM84','DIM85','DIM86','DIM87','DIM88','DIM89','DIM90']
 
+#For excluding any month, please delete their value and separator within variable "list_of_months".
+list_of_months = '1.,2.,3.,4.,5.,6.,7.,8.,9.,10.,11.,12.'
+
+#For excluding any indicator, please delete their value and separator within variable "list_of_indicators".
+list_of_indicators = 'UKAZ04, UKAZ07, UKAZ10'
+#List of indicators for the given data topic may be found: https://data.statistics.sk/api/v2/dimension/cr3804mr/cr3804mr_ukaz?lang=en
+
+#For excluding any dimension (incoming market), please delete their value and separator within variable "list_of_dims".
+list_of_dims = ['DIM01','DIM02','DIM03','DIM04','DIM05','DIM06','DIM07','DIM08','DIM09','DIM10','DIM11','DIM12','DIM13','DIM14','DIM15','DIM16','DIM17','DIM18','DIM19','DIM20','DIM21','DIM22','DIM23','DIM24','DIM25','DIM26','DIM27','DIM28','DIM29','DIM30','DIM31','DIM32','DIM33','DIM34','DIM35','DIM36','DIM37','DIM38','DIM39','DIM40','DIM41','DIM42','DIM43','DIM44','DIM45','DIM46','DIM47','DIM48','DIM49','DIM50','DIM51','DIM52','DIM53','DIM54','DIM55','DIM56','DIM57','DIM58','DIM59','DIM60','DIM61','DIM62','DIM63','DIM64','DIM65','DIM66','DIM67','DIM68','DIM69','DIM70','DIM71','DIM72','DIM73','DIM74','DIM75','DIM76','DIM77','DIM78','DIM79','DIM80','DIM81','DIM82','DIM83','DIM84','DIM85','DIM86','DIM87','DIM88','DIM89','DIM90']
+#List of dimension for the given data topic may be found: https://data.statistics.sk/api/v2/dimension/cr3804mr/cr3804mr_dim3?lang=en
 
 
 def get_tab(x,y):  
@@ -52,14 +54,13 @@ def get_tab(x,y):
     df_label = json_stat_dateset.to_data_frame()
     final_data = pd.merge(df_idx, df_label, left_index=True, right_index=True)
     print(final_data)
-    #For exporting data into SQL or PostgreSql DB, please use the command below by deleting the hashtag #.
+#For exporting data into SQL or PostgreSql DB, please use the command below by deleting the hashtag #.
     #final_data.to_sql('inputCr3804mr', engine, if_exists='append')
-    #For exporting data into a .CSV, please use the command below by deleting the hashtag #.
+#For exporting data into a .CSV, please use the command below by deleting the hashtag #.
     #final_data.to_excel('inputCr3804mr.xlsx')
     
 x=0
 y=0
-
 
 get_tab(x,y)
 
