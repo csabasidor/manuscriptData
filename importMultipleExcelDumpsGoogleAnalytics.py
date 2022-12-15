e3fe86b54a899962d099691b26775737ee21dc02
cs_sido <csaba.sidor@gmail.com>
2022-12-15 10:04:06 +0100

    Update importMultipleExcelDumpsGoogleAnalytics.py

--- importMultipleExcelDumpsGoogleAnalytics.py
+++ importMultipleExcelDumpsGoogleAnalytics.py
@@ -21,42 +21,100 @@ import psycopg2 as pg
 from sqlalchemy import create_engine, MetaData, Table
 import pandas as pd
 import pandasql as ps
+import os
 
 
+global listOfFilePathsCityData
+listOfFilePathsCityData = []
 
-list_of_files=['C:\\Users\\csb\\Documents\\csb_files\\00notes\\cc_ors\\slr_ga_month_21\\Analytics Všetky údaje webových stránok Location 20210101-20210131.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\00notes\\cc_ors\\slr_ga_month_21\\Analytics Všetky údaje webových stránok Location 20210201-20210228.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\00notes\\cc_ors\\slr_ga_month_21\\Analytics Všetky údaje webových stránok Location 20210301-20210331.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\00notes\\cc_ors\\slr_ga_month_21\\Analytics Všetky údaje webových stránok Location 20210401-20210430.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\00notes\\cc_ors\\slr_ga_month_21\\Analytics Všetky údaje webových stránok Location 20210501-20210531.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\00notes\\cc_ors\\slr_ga_month_21\\Analytics Všetky údaje webových stránok Location 20210601-20210630.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\00notes\\cc_ors\\slr_ga_month_21\\Analytics Všetky údaje webových stránok Location 20210701-20210731.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\00notes\\cc_ors\\slr_ga_month_21\\Analytics Všetky údaje webových stránok Location 20210801-20210831.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\00notes\\cc_ors\\slr_ga_month_21\\Analytics Všetky údaje webových stránok Location 20210901-20210930.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\00notes\\cc_ors\\slr_ga_month_21\\Analytics Všetky údaje webových stránok Location 20211001-20211031.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\00notes\\cc_ors\\slr_ga_month_21\\Analytics Všetky údaje webových stránok Location 20211101-20211130.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\00notes\\cc_ors\\slr_ga_month_21\\Analytics Všetky údaje webových stránok Location 20211201-20211231.xlsx']
-               #, 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20180201-20180228.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20180301-20180331.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20180401-20180430.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20180501-20180531.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20180601-20180630.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20180701-20180731.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20180801-20180831.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20180901-20180930.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20181001-20181031.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20181101-20181130.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20181201-20181231.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20190101-20190131.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20190201-20190228.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20190301-20190331.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20190401-20190430.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20190501-20190531.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20190601-20190630.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20190701-20190731.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20190801-20190831.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20190901-20190930.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20191001-20191031.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20191101-20191130.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20191201-20191231.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20200101-20200131.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20200201-20200229.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20200301-20200331.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20200401-20200430.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20200501-20200531.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20200601-20200630.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20200701-20200731.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20200801-20200831.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20200901-20200930.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20201001-20201031.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20201101-20201130.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20201201-20201231.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20210101-20210131.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20210201-20210228.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20210301-20210331.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20210401-20210430.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20210501-20210531.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20210601-20210630.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20210701-20210731.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20210801-20210831.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20210901-20210930.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20211001-20211031.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20211101-20211130.xlsx', 'C:\\Users\\csb\\Documents\\csb_files\\viske2021\\Analytics All Web Site Data Location 20211201-20211231.xlsx']
 
-global list_df
-list_df = []
+global listOfFilePathsCountryData
+listOfFilePathsCountryData = []
 
-x = 0
 
-def foo():
+def list_files(dir):
     
-    df = pd.read_excel(list_of_files[x], sheet_name='Dataset1')
+    for root, dirs, files in os.walk(dir):
+        
+        print(root)
+        #print(dirs)
+        #print(files)
+        for name in files:
+            if 'GAcountry' in str(root):
+                listOfFilePathsCountryData.append(os.path.join(root, name))
+            elif 'GAcity' in str(root):
+                listOfFilePathsCityData.append(os.path.join(root, name))
+            else:
+                pass
+
+#INSERT PATH TO DIR GAcountry containing monthly country metrics
+# e.g. C:\\Users\\Admin\\datadumps\\GAcountry
+list_files('\\GAcountry')
+#INSERT PATH TO DIR GAccity containing monthly country metrics
+# e.g. C:\\Users\\Admin\\datadumps\\GAcity
+list_files('\\GAcity')
+
+
+
+global list_df_city
+list_df_city = []
+
+def cityData():
+    
+    df = pd.read_excel(listOfFilePathsCityData[x], sheet_name='Dataset1')
+    #The name of the files in d_range may vary depending on Your language
     d_range = list_of_files[x].split("Analytics Všetky údaje webových stránok Location ")[1].split('.xlsx')[0]
     d_year = d_range[0:4]
     d_month = d_range[4:6]
     df.loc[:, 'd_year'] = d_year
     df.loc[:, 'd_month'] = d_month
-    df.loc[:, 'page_name'] = 'Visit Kosice'
-    df_out = df[['page_name','d_year', 'd_month','City', 'Country', 'Users', 'New Users', 'Sessions', 'Bounce Rate', 'Pages / Session', 'Avg. Session Duration']]
-    list_df.append(df_out)
-    #print(df_out.head)
+    #INSERT PAGE NAME
+    df.loc[:, 'page_name'] = 'PAGE NAME'
+    df_out = df[['page_name','d_year', 'd_month','City', 'Country', 'Users', 'New Users', 'Sessions', 'Avg. Session Duration']]
+    list_df_city.append(df_out)
+    
+x = 0
+cityData()
 
-foo()
-
-while x < len(list_of_files) - 1:
+while x < len(listOfFilePathsCityData) - 1:
     x =x +1
-    foo()
+    cityData()
 
-data = pd.concat(list_df)
+dataCity = pd.concat(list_df_city)
+dataCity.reset_index(level=0, inplace=False)
 
-data.reset_index(level=0, inplace=False)
+
+global list_df_country
+list_df_country = []
+
+def countryData():
+    
+    df = pd.read_excel(listOfFilePathsCountryData[x], sheet_name='Dataset1')
+    #The name of the files in d_range may vary depending on Your language
+    d_range = list_of_files[x].split("Analytics Všetky údaje webových stránok Location ")[1].split('.xlsx')[0]
+    d_year = d_range[0:4]
+    d_month = d_range[4:6]
+    df.loc[:, 'd_year'] = d_year
+    df.loc[:, 'd_month'] = d_month
+    #INSERT PAGE NAME
+    df.loc[:, 'page_name'] = 'PAGE NAME'
+    df_out = df[['page_name','d_year', 'd_month', 'Country', 'Users', 'New Users', 'Sessions', 'Avg. Session Duration']]
+    list_df_country.append(df_out)
+    
+x = 0
+countryData()
+
+while x < len(listOfFilePathsCountryData) - 1:
+    x =x +1
+    countryData()
+
+dataCountry = pd.concat(list_df_country)
+dataCountry.reset_index(level=0, inplace=False)
 
 
-#ENTER DB CREDENTIALS for LOADING into DB
-engine = create_engine('postgresql+psycopg2://USERNAME:PASSWORD@HOSTNAME:PORTNUMBER/DBNAME')
+#For loading the response data into a PostrgeSQL DB, please insert necessary credentials into the vaiable "engine" and remove the hashtag #.
+#engine = create_engine('postgresql+psycopg2://USERNAME:PASSWORD@HOSTNAME:PORTNUMBER/DBNAME')
 print("contected")
-data.to_sql("ga_city_month", engine, if_exists='append')
+dataCity.to_sql("ga_city_month", engine, if_exists='append')
+dataCountry.to_sql("ga_country_month", engine, if_exists='append')
+
