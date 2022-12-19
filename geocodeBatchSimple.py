bfb5856597572378cff3a128da269eb228ea3e12
cs_sido <csaba.sidor@gmail.com>
2022-12-19 13:44:01 +0100

    Update geocodeBatchSimple.py

--- geocodeBatchSimple.py
+++ geocodeBatchSimple.py
@@ -9,16 +9,16 @@ pd.set_option('display.width', 1500)
 startTime = datetime.now()
 import pandas.io.sql as psql
 import psycopg2 as pg
-from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy import create_engine, MetaData, Table, text
 import time
 
 
 #CREATE A CONNECTION WITH DB, please insert necessary credentials into the vaiable "engine" and remove the hashtag #.
 #conn_1 = pg.connect("dbname= DBNAME user=USER host=HOST port=PORT password=PASSWORD")
 
+
 #call all records without coordinates
-#DEFAULT LIMIT 50 for API LIMITATIONS
-call_df_in = psql.read_sql("SELECT *, origin_city || ', ' || origin_country adr_in FROM ga_fb_geocoded_cities WHERE adr IS NULL LIMIT 50", conn_1)
+call_df_in = psql.read_sql("SELECT *, origin_city || ', ' || origin_country adr_in FROM ga_fb_geocoded_cities WHERE adr IS NULL LIMIT 5", conn_1)
 df_in = pd.DataFrame(call_df_in)
 
 conn_1.close()
@@ -30,19 +30,19 @@ list_df_out = []
 
 #Define calls a data manupilation for ORS API
 def dataparser(x):
-#INSERT YOUR API KEY
-    print('https://api.openrouteservice.org/geocode/search?api_key=_INSERT_API_KEY_HERE_&text=' + str(df_in['adr_in'][x]))
+
+    print('https://api.openrouteservice.org/geocode/search?api_key=5b3ce3597851110001cf62488eb6c3ab311a4c46b02607a24bf3a792&text=' + str(df_in['adr_in'][x]))
     headers = {
         'Accept': 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
     }
-    #INSERT YOUR API KEY
-    call = requests.get('https://api.openrouteservice.org/geocode/search?api_key=_INSERT_API_KEY_HERE_&text=' + str(df_in['adr_in'][x]), headers=headers)
+    call = requests.get('https://api.openrouteservice.org/geocode/search?api_key=5b3ce3597851110001cf62488eb6c3ab311a4c46b02607a24bf3a792&text=' + str(df_in['adr_in'][x]), headers=headers)
 
     
     print(call.status_code, call.reason)
 
     js = call.json()
 
+    
     
 
     
@@ -84,7 +84,7 @@ def dataparser(x):
                 dict_flagged = { "type": "n/a", "adr": str(df_in['adr_in'][x]), "coordinates": ["n/a"], "ors_id": "n/a", "osm_id": "n/a", "type": "n/a", "city_name": "n/a", "region_name": "n/a", "country_name": "n/a"}
                 df = pd.DataFrame.from_dict(dict_flagged)
                 list_df_out.append(df)
-             
+            
 
     
     if not js['features']:
@@ -105,7 +105,6 @@ while x < len(df_in['adr_in']) - 1:
     x = x + 1
     dataparser(x)
 
-
 data_out = pd.concat(list_df_out)
 data_out = data_out.reset_index()
 
@@ -113,16 +112,14 @@ data_out = data_out.reset_index()
 #For loading the response data into a PostrgeSQL DB, please insert necessary credentials into the vaiable "engine" and remove the hashtag #.
 #engine = create_engine('postgresql+psycopg2://USERNAME:PASSWORD@HOSTNAME:PORTNUMBER/DBNAME')
 
-#EXPORT TEMP TABLE WITH COORDINATES
-data_out.to_sql('fbgacit', engine, if_exists='replace')
 
+update_query = "UPDATE ga_fb_geocoded_cities set \"type\" ='" + data_out['type'] +"', coordinates = '" + data_out['coordinates'].apply(str) +"', adr = '" + data_out['adr'] +"', geometry_confidence = '" + data_out['geometry_confidence'].apply(str) +"', ors_id = '"+ data_out['ors_id']+ "', osm_id = '"+ data_out['osm_id']+ "', osm_layer = '"+ data_out['osm_layer']+ "', city_name ='"+ data_out['region_name']+ "', region_name = '" + data_out['region_name']+ "', country_name ='" + data_out['country_name']+ "' WHERE origin_city || ', ' || origin_country ='" +data_out['adr']+"';"
 
-#UPDATE COORDINATES IN DB
-with engine.connect() as con:
-    query = "UPDATE ga_fb_geocoded_cities set \"type\" = nv.\"type\", coordinates = nv.coordinates, adr = nv.adr, geometry_confidence = nv.geometry_confidence, ors_id = nv. ors_id, osm_id = nv.osm_id, osm_layer = nv.osm_layer, city_name = nv.city_name, region_name = nv.region_name, country_name = nv.country_name FROM (SELECT * FROM fbgacit) nv WHERE origin_city || ', ' || origin_country = nv.adr"
-    con.execute(query)
+for item in update_query:
+    with engine.begin() as conn:     
+        conn.execute(text(item))
 
-con.close()
+conn.close()
 
 
 
